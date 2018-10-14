17552497fe5d7f910e7d7f8dee9f014e46871ea3
苏瑞辅 Su Ruifu <rvfu98@gmail.com>
2018-10-14 15:47:04 +0800

    Updated
    优化更新代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,187 +1,35 @@
-Date.prototype.pattern = function (fmt) {
-  var o = {
-    "M+": this.getMonth() + 1, //月份         
-    "d+": this.getDate(), //日         
-    "h+": this.getHours() % 12 == 0 ? 12 : this.getHours() % 12, //小时         
-    "H+": this.getHours(), //小时         
-    "m+": this.getMinutes(), //分         
-    "s+": this.getSeconds(), //秒         
-    "q+": Math.floor((this.getMonth() + 3) / 3), //季度         
-    "S": this.getMilliseconds() //毫秒         
-  };
-  var week = {
-    "0": "1",
-    "1": "0",
-    "2": "0",
-    "3": "0",
-    "4": "0",
-    "5": "0",
-    "6": "1"
-  };
-  if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-  }
-  if (/(E+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? "/u661f/u671f" : "/u5468") : "") + week[this.getDay() + ""]);
-  }
-  for (var k in o) {
-    if (new RegExp("(" + k + ")").test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    }
-  }
-  return fmt;
+// 引入数据文件
+var data = require('./data.js'); // Import data from data.js
+
+// 判断是否是工作日
+var is_workday = function(date){
+  var day = date.getDay()
+  return !(day==0|day==6);
 }
 
+// 获取当前时间[月份，日期，是否是节假日]
+var setTime = function(date=null){
+  var date = date?(new Date(date)):(new Date())
+  var time = {};
+  time.month = date.getMonth() + 1;
+  time.date = date.getDate();
+  time.is_workday = is_workday(date);
+  this.setData({
+    time: time
+  })
+}
 
-// pages/index/index.js
+// 初始化页面
 Page({
-
-  /**
-   * Page initial data
-   */
+  setTime: setTime,
   data: {
-    date: '',
-    table_data: [[[
-      { time: "07:05", count: 1, dq: true },
-      { time: "07:15", count: 1, dq: false },
-      { time: "08:05", count: 1, dq: true },
-      { time: "08:10", count: 1, dq: false },
-      { time: "09:10", count: 1, dq: true },
-      { time: "10:10", count: 1, dq: true },
-      { time: "11:05", count: 1, dq: true },
-      { time: "12:10", count: 1, dq: true },
-      { time: "12:15", count: 1, dq: false },
-      { time: "13:15", count: 1, dq: true },
-      { time: "13:20", count: 1, dq: false },
-      { time: "14:05", count: 1, dq: true },
-      { time: "14:55", count: 1, dq: true },
-      { time: "15:00", count: 1, dq: false },
-      { time: "16:00", count: 1, dq: true },
-      { time: "16:05", count: 1, dq: true },
-      { time: "16:10", count: 1, dq: false },
-      { time: "17:00", count: 1, dq: true },
-      { time: "17:35", count: 1, dq: true },
-      { time: "17:40", count: 1, dq: true },
-      { time: "17:40", count: 1, dq: false },
-      { time: "17:45", count: 1, dq: true },
-      { time: "18:15", count: 1, dq: true },
-      { time: "18:25", count: 1, dq: false },
-      { time: "20:10", count: 1, dq: true },
-      { time: "21:05", count: 1, dq: true },
-      { time: "22:10", count: 1, dq: true }],
-    [
-      { time: "06:50", count: 1, dq: true },
-      { time: "07:00", count: 2, dq: true },
-      { time: "07:05", count: 2, dq: true },
-      { time: "07:10", count: 1, dq: true },
-      { time: "08:05", count: 1, dq: true },
-      { time: "08:10", count: 1, dq: true },
-      { time: "08:20", count: 2, dq: true },
-      { time: "09:10", count: 2, dq: true },
-      { time: "09:20", count: 1, dq: true },
-      { time: "10:30", count: 1, dq: true },
-      { time: "11:10", count: 1, dq: true },
-      { time: "12:10", count: 1, dq: true },
-      { time: "13:10", count: 2, dq: true },
-      { time: "13:20", count: 1, dq: true },
-      { time: "15:20", count: 2, dq: true },
-      { time: "16:00", count: 1, dq: true },
-      { time: "17:20", count: 1, dq: true },
-      { time: "17:35", count: 1, dq: true },
-      { time: "17:40", count: 1, dq: false },
-      { time: "18:10", count: 1, dq: true },
-      { time: "19:10", count: 1, dq: true },
-      { time: "20:25", count: 1, dq: true },
-      { time: "21:40", count: 1, dq: true }]],
-    [
-      [
-        { time: "08:05", count: 1, dq: true },
-        { time: "08:10", count: 1, dq: false },
-        { time: "12:10", count: 1, dq: true },
-        { time: "12:15", count: 1, dq: false },
-        { time: "16:55", count: 1, dq: true },
-        { time: "17:00", count: 1, dq: false },
-        { time: "18:15", count: 1, dq: true },
-        { time: "20:10", count: 1, dq: true },
-        { time: "21:05", count: 1, dq: true },
-        { time: "22:10", count: 1, dq: true }],
-      [
-        { time: "07:15", count: 1, dq: true },
-        { time: "07:20", count: 1, dq: true },
-        { time: "09:15", count: 1, dq: true },
-        { time: "09:20", count: 1, dq: true },
-        { time: "13:15", count: 1, dq: true },
-        { time: "13:20", count: 1, dq: true },
-        { time: "17:40", count: 1, dq: true },
-        { time: "19:30", count: 1, dq: true },
-        { time: "20:25", count: 1, dq: true },
-        { time: "21:40", count: 1, dq: true }]]]
-
+    time:{},
+    bus_time_table: data.bus_time_table
   },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
-    var date = (new Date()).pattern("yyyy-MM-dd E");
-    // var date = new Date().Format("yyyy-MM-dd");
-    this.setData({
-      date: date
-    })
+  onShow: function (options) {
+    var time = this.setTime();
   },
   bindDateChange: function (e) {
-    var time = (new Date(e.detail.value)).pattern("yyyy-MM-dd E");
-    this.setData({
-      date: time
-    })
-  },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
+    var time = this.setTime(e.detail.value);
   }
 })
